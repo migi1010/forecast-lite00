86f4fabab0bf1616b35654f74e8204da57fceef5
chang kuan i <a0975825061@gmail.com>
2025-09-13 21:44:02 +0800

    model_user_rf.py

--- models/model_user_rf.py
+++ models/model_user_rf.py
@@ -1,8 +1,9 @@
+# models/model_user_rf.py
 import numpy as np
 from datetime import datetime, timedelta
-from models.utils import fetch_xauusd_history  # 注意這裡要 models.utils
+from .utils import fetch_xauusd_history
 
-# Dummy 隨機森林模型
+# 你的 RF 模型
 class DummyRF:
     def predict(self, X):
         return X[:,-1,0] + np.random.normal(0, 1.5, size=X.shape[0])
@@ -11,7 +12,7 @@ model = DummyRF()
 
 def run_prediction(symbol, sample_size, horizon_days=7, random_seed=None):
     np.random.seed(random_seed or 42)
-    last_prices = fetch_xauusd_history()['Close'].values[-sample_size:] if symbol=="XAUUSD" else np.ones(sample_size)*1.25
+    last_prices = fetch_xauusd_history()['Close'].values[-sample_size:]
     X = last_prices.reshape(1, sample_size, 1)
     y_pred = model.predict(X)
     
